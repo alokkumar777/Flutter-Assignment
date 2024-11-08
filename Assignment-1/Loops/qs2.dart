d1c27ee3eb23a74be6fc347f92a6c59cd3d7b6b5
Alok Kumar <136910791+alokkumar777@users.noreply.github.com>
2024-11-08 19:05:00 +0530

    Added loops questions

--- Assignment-1/Loops/qs2.dart
+++ Assignment-1/Loops/qs2.dart
@@ -3,5 +3,11 @@
 import 'dart:io';
 
 void main() {
-  
+  stdout.write("Enter number to get sum: ");
+  int N = int.parse(stdin.readLineSync()!);
+  int sum = 0;
+  for (int i = 1; i <= N; i++) {
+    sum += i;
+  }
+  print(sum);
 }
